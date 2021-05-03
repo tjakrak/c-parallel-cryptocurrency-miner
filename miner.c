9e3f290fbec4199b5b1ff3dad1d86458494c1e3a
tjakrak <rgtjakrakartadinata@dons.usfca.edu>
2021-05-03 12:09:20 -0700

    pthreadjoin lead to an error (possibly deadlock)

--- miner.c
+++ miner.c
@@ -36,7 +36,26 @@
 
 #include "sha1.h"
 
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t condc = PTHREAD_COND_INITIALIZER;
+pthread_cond_t condp = PTHREAD_COND_INITIALIZER;
+
+
 unsigned long long total_inversions;
+char *bitcoin_block_data;
+int num_threads;
+uint32_t difficulty_mask = 0;
+
+
+int buffer = 0;
+uint64_t last_index = 0;
+struct tasks {
+    uint64_t start;
+    uint64_t end;
+} *tsk;
+
+uint64_t nonce_result = 0;
+char solution[41];
 
 double get_time()
 {
@@ -67,6 +86,7 @@ uint64_t mine(char *data_block, uint32_t difficulty_mask,
          * For example, if we have 'Hello World!' and '10', the new string
          * is: 'Hello World!10' */
         snprintf(buf, buf_sz, "%s%lu", data_block, nonce);
+        //printf("%s\n", buf);
 
         /* Hash the combined string */
         sha1sum(digest, (uint8_t *) buf, strlen(buf));
@@ -89,56 +109,203 @@ uint64_t mine(char *data_block, uint32_t difficulty_mask,
     return 0;
 }
 
-int main(int argc, char *argv[]) {
+void *consumer_thread(void *ptr) {
 
+    while (last_index < UINT64_MAX / 100 / num_threads && nonce_result == 0) {
+        pthread_mutex_lock(&mutex);
+        //int x = (int) ptr;
+
+        //struct tasks *t = (struct tasks*) ptr;
+        while (buffer < 1 || last_index >= UINT64_MAX / 100 / num_threads) {
+            if (last_index >= UINT64_MAX / 100 / num_threads || nonce_result != 0) {
+                //printf("Threads %d EXIT HERE\n", x);
+                printf("Last Index = %lu\n", last_index); 
+                //pthread_cond_signal(&condp);
+                pthread_mutex_unlock(&mutex); 
+                return 0;
+            }
+
+            pthread_cond_wait(&condc, &mutex);
+
+        }
+        
+        int real_index = last_index % 100;
+        printf("Struct value: %lu, %lu\n", tsk[real_index].start, tsk[real_index].end);
+        //printf("Threads: %d Struct value: %lu, %lu\n", x, tsk[real_index].start, tsk[real_index].end);
+
+        uint64_t start = tsk[real_index].start;
+        uint64_t end = tsk[real_index].end;
+
+        last_index++;
+        buffer--; // Deceremented the buffer we have (total number of struct in the array)
+
+        pthread_cond_signal(&condc);
+        pthread_mutex_unlock(&mutex);
+        
+        uint8_t digest[SHA1_HASH_SIZE];
+
+        /* Mine the block. */
+        uint64_t nonce = mine(
+            bitcoin_block_data,
+            difficulty_mask,
+            start, end,
+            digest); 
+
+        char solution_hash[41];
+        sha1tostring(solution_hash, digest);
+        
+        pthread_mutex_lock(&mutex);
+        //sleep(1);
+        if (nonce != 0) {
+            if (nonce_result == 0 || nonce < nonce_result) {
+                strcpy(solution, &solution_hash[0]);
+                nonce_result = nonce;
+            }
+            pthread_cond_broadcast(&condc);
+            printf("Hash: %s\n", solution_hash);
+//            pthread_mutex_unlock(&mutex);
+//            return 0;
+        }
+        
+        printf("lastindex: %lu\n", last_index);
+        printf("counter= %d\n", buffer);
+        
+        /* Wake up the producer */
+        pthread_cond_signal(&condp);
+        pthread_mutex_unlock(&mutex);
+    }
+
+    return 0;
+}
+
+
+int main(int argc, char *argv[]) {
+    
     if (argc != 4) {
         printf("Usage: %s threads difficulty 'block data (string)'\n", argv[0]);
         return EXIT_FAILURE;
     }
 
-    int num_threads = 1; // TODO
+    // convert number of threads from string to int
+    num_threads = (int)strtol(argv[1], NULL, 10);
+
+    if (num_threads < 1) {
+        return 1;
+    }
+
     printf("Number of threads: %d\n", num_threads);
 
-    // TODO we have hard coded the difficulty to 20 bits (0x0000FFF). This is a
+    // TODO we have hard coded the difficulty to 20 bits (0x00000FFF). This is a
     // fairly quick computation -- something like 28 will take much longer.  You
     // should allow the user to specify anywhere between 1 and 32 bits of zeros.
-    uint32_t difficulty_mask = 0x00000FFF;
+    
+    int num_difficulty = (int)strtol(argv[2], NULL, 10);
+    
+    for (int i = 0; i < 32 - num_difficulty; i++) {
+        difficulty_mask = difficulty_mask | 1 << i;
+    }
+    
     printf("  Difficulty Mask: ");
     print_binary32(difficulty_mask);
+    
 
     /* We use the input string passed in (argv[3]) as our block data. In a
      * complete bitcoin miner implementation, the block data would be composed
      * of bitcoin transactions. */
-    char *bitcoin_block_data = argv[3];
+    bitcoin_block_data = argv[3];
     printf("       Block data: [%s]\n", bitcoin_block_data);
 
     printf("\n----------- Starting up miner threads!  -----------\n\n");
 
     double start_time = get_time();
 
-    uint8_t digest[SHA1_HASH_SIZE];
+    // generate little task (creating a struct)
+    // struct - difficulty, block data, nonce range
+    tsk = malloc(100 * sizeof(struct tasks));
+    
+    int range = 100;
 
-    /* Mine the block. */
-    uint64_t nonce = mine(
-            bitcoin_block_data,
-            difficulty_mask,
-            1, UINT64_MAX,
-            digest);
+    pthread_t *consumers = malloc(sizeof(pthread_t) * 5);
+    int i;
+    for (i = 0; i < num_threads; i++) {
+        //pthread_create(&consumers[i], NULL, consumer_thread, (void *) i);
+        pthread_create(&consumers[i], NULL, consumer_thread, NULL);
+    }
+
+    // Producer
+    uint64_t j = 0;    
+    while (j < UINT64_MAX && nonce_result == 0) {
+        j++;
+
+        pthread_mutex_lock(&mutex);
+
+        while (buffer >= 100 || nonce_result != 0) {
+            if (nonce_result != 0) {
+                break;
+            }
+ 
+            pthread_cond_wait(&condp, &mutex);
+
+//            if (nonce_result != 0) {
+//                break;
+//            }
+        }       
+        
+        int idx = (j - 1) % 100;
+        
+//        printf("INDEX = %d\n", idx);
+        tsk[idx].start = (j - 1) * range + 1;
+        tsk[idx].end = tsk[idx].start + range - 1;
+        
+        printf("start = %lu, end = %lu\n", tsk[idx].start, tsk[idx].end);
+
+        buffer++;
+//        printf("buffer = %d\n", buffer);
+       
+        pthread_cond_signal(&condc); 
+        pthread_mutex_unlock(&mutex);
+    }
+
+
+
+    int k;
+    for (k = 0; k < num_threads; k++) {
+        printf("join: %d\n", k);
+        pthread_join(consumers[k], NULL);
+    }
+
+
+    printf("final buffer= %d\n", buffer);
+    //free(consumers);
+    //free(tsk);
+
+
+
+//    uint8_t digest[SHA1_HASH_SIZE];
+//
+//    /* Mine the block. */
+//    uint64_t nonce = mine(
+//            bitcoin_block_data,
+//            difficulty_mask,
+//            1, UINT64_MAX,
+//            digest);
 
     double end_time = get_time();
 
-    if (nonce == 0) {
-        printf("No solution found!\n");
-        return 1;
-    }
+//    if (nonce == 0) {
+//        printf("No solution found!\n");
+//        return 1;
+//    }
 
     /* When printed in hex, a SHA-1 checksum will be 40 characters. */
-    char solution_hash[41];
-    sha1tostring(solution_hash, digest);
+//    char solution_hash[41];
+//    sha1tostring(solution_hash, digest);
 
     printf("Solution found by thread %d:\n", 0);
-    printf("Nonce: %lu\n", nonce);
-    printf(" Hash: %s\n", solution_hash);
+    printf("Nonce: %lu\n", nonce_result);
+//    printf(" Hash: %s\n", solution_hash);
+
+    printf("Solution hash %s\n", solution);
 
     double total_time = end_time - start_time;
     printf("%llu hashes in %.2fs (%.2f hashes/sec)\n",
